2e701d3282b1176c9c74b23496e2dc482402e55e
HQAQ <1401651730@qq.com>
2022-05-08 22:41:03 +0800

    iml

--- conCurrentHashMap/src/main/java/com/test/Test.java
+++ conCurrentHashMap/src/main/java/com/test/Test.java
@@ -1,6 +1,6 @@
 package com.test;
 
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.HashMap;
 
 /**
  * @program: java-coding
@@ -13,5 +13,10 @@ public class Test {
     public static void main(String[] args) {
        // ConcurrentHashMap<Integer,Integer> res = new ConcurrentHashMap<Integer, Integer>();
         System.out.println(Runtime.getRuntime().availableProcessors());
+
+        HashMap<Integer,String> map = new HashMap<>(10);
+        map.put(1,"2");
+        map.put(17,"3");
+        map.put(2,"4");
     }
 }
