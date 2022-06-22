ba2527421cf0c6a0b31ec653be0cb6a7f59ef779
heqiang1234 <1401651730@qq.com>
2022-06-23 00:23:11 +0800

    二分第二遍  611待续

---
+++ Leet-code/src/main/java/com/leetcode/Algorithm/TwoOrderSearch/Question611_1TriangleNumber.java
@@ -0,0 +1,15 @@
+package com.leetcode.Algorithm.TwoOrderSearch;
+
+/**
+ * @author HQ
+ * @program: java-coding
+ * @description: 611. 有效三角形的个数
+ * @date 2022-06-22 22:33:56
+ */
+public class Question611_1TriangleNumber {
+
+    //我的想法，找第三条边，两边之和大于第三边，两边之差，小于第三条边。
+    public int triangleNumber(int[] nums) {
+
+    }
+}
