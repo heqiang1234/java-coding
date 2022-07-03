fdbebf6db8b1e4f12611231f32b15d6594ee8b77
heqiang1234 <1401651730@qq.com>
2022-07-04 01:51:55 +0800

    arraylist学习module

---
+++ javaBade/src/main/java/hq.arrayslist/ArraysListTtest.java
@@ -0,0 +1,18 @@
+package hq.arrayslist;
+
+import java.util.ArrayList;
+
+/**
+ * @author HQ
+ * @program: java-coding
+ * @description: arraysList学习
+ * @date 2022-06-30 23:59:51
+ */
+public class ArraysListTtest {
+
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        //list.ensureCapacity();
+    }
+}
