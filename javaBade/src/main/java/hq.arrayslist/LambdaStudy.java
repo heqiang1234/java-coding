fc3a55a6b504bcf4652c95ac89be8c2d23f52812
heqiang1234 <1401651730@qq.com>
2022-07-06 23:06:48 +0800

    1

---
+++ javaBade/src/main/java/hq.arrayslist/LambdaStudy.java
@@ -0,0 +1,14 @@
+package hq.arrayslist;
+
+/**
+ * @author HQ
+ * @program: java-coding
+ * @description: lambda学习
+ * @date 2022-07-04 23:51:30
+ */
+public class LambdaStudy {
+
+    public static void main(String[] args) {
+       // (x, y) -> x + y;
+    }
+}
