3ad3e40b83cea2a63febb3563c0195ec50e429e4
heqiang1234 <1401651730@qq.com>
2022-07-27 23:56:07 +0800

    实现跳表

---
+++ Leet-code/src/main/java/com/leetcode/sginin/Question1206Skiplist.java
@@ -0,0 +1,39 @@
+package com.leetcode.sginin;
+
+import com.leetcode.model.ListNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+/**
+ * @author HQ
+ * @program: java-coding
+ * @description: 1206.跳表
+ * @date 2022-07-26 21:02:55
+ */
+public class Question1206Skiplist {
+
+    class skipNode {
+        int value;
+        ArrayList<skipNode> nextNode;
+    }
+
+    ListNode res;
+
+    public Question1206Skiplist() {
+        res = new ListNode();
+    }
+
+    public boolean search(int target) {
+        return false;
+    }
+
+    public void add(int num) {
+
+    }
+
+    public boolean erase(int num) {
+        return false;
+    }
+
+}
