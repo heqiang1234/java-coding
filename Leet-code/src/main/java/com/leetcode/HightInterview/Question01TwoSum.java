49b77d2f2f3912f7836a81712b9d629b6be1f222
heqiang1234 <1401651730@qq.com>
2022-08-09 23:56:47 +0800

    高效制胜

---
+++ Leet-code/src/main/java/com/leetcode/HightInterview/Question01TwoSum.java
@@ -0,0 +1,15 @@
+package com.leetcode.HightInterview;
+
+/**
+ * @author HQ
+ * @program: java-coding
+ * @description: 1.两数之和
+ * @date 2022-08-09 23:50:19
+ */
+public class Question01TwoSum {
+
+    public int[] twoSum(int[] nums, int target) {
+
+    }
+
+}
