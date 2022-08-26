f8738f15556d928c89eee6eccd99a1b37503166b
HQAQ <1401651730@qq.com>
2022-08-26 17:32:07 +0800

    高效题目，第二天

---
+++ Leet-code/src/main/java/com/leetcode/HightInterview/Day2_Question18FourSum.java
@@ -0,0 +1,40 @@
+package com.leetcode.HightInterview;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author: Mr.He
+ * @create: 2022-08-26 16:52
+ * @description: 18.四数之和
+ *
+ * 给你一个由 n 个整数组成的数组 nums ，和一个目标值 target 。请你找出并返回满足下述全部条件且不重复的四元组 [nums[a], nums[b], nums[c], nums[d]] （若两个四元组元素一一对应，则认为两个四元组重复）：
+ *
+ * 0 <= a, b, c, d < n
+ * a、b、c 和 d 互不相同
+ * nums[a] + nums[b] + nums[c] + nums[d] == target
+ * 你可以按 任意顺序 返回答案 。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [1,0,-1,0,-2,2], target = 0
+ * 输出：[[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+ * 示例 2：
+ *
+ * 输入：nums = [2,2,2,2,2], target = 8
+ * 输出：[[2,2,2,2]]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/4sum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ **/
+public class Day2_Question18FourSum {
+
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        
+    }
+
+}
