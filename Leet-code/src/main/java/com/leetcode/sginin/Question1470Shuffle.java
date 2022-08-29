8325c15afba8697b3cc88cde5d7b58b081b721a9
heqiang1234 <1401651730@qq.com>
2022-08-29 22:39:13 +0800

    借用了额外的空间，可以试下原地修改，有一位大佬说高位和低位存储。无敌

--- Leet-code/src/main/java/com/leetcode/sginin/Question1470Shuffle.java
+++ Leet-code/src/main/java/com/leetcode/sginin/Question1470Shuffle.java
@@ -7,4 +7,21 @@ package com.leetcode.sginin;
  * @date 2022-08-29 21:17:55
  */
 public class Question1470Shuffle {
+
+    public static int[] shuffle(int[] nums, int n) {
+        int len = nums.length;
+        int [] res = new int[len];
+        int a = nums[0],b = nums[1];
+        for (int i = 0; i < n; i++) {
+            a = nums[i + 1];
+            b = nums[i + 2];
+
+        }
+        return nums;
+    }
+
+    public static void main(String[] args) {
+        shuffle(new int[]{2,5,1,3,4,7}, 3);
+    }
+
 }
