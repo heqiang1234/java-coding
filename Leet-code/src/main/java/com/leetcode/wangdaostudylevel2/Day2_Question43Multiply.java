b5e6bb6eaa6daf7dab826df7ff9e1763d3da6075
heqiang1234 <1401651730@qq.com>
2022-08-31 23:08:13 +0800

    字符串的乘积

--- Leet-code/src/main/java/com/leetcode/wangdaostudylevel2/Day2_Question43Multiply.java
+++ Leet-code/src/main/java/com/leetcode/wangdaostudylevel2/Day2_Question43Multiply.java
@@ -1,6 +1,7 @@
 package com.leetcode.wangdaostudylevel2;
 
 import io.netty.util.internal.StringUtil;
+import org.springframework.web.servlet.support.AbstractFlashMapManager;
 
 import java.util.Arrays;
 import java.util.stream.Stream;
@@ -13,29 +14,51 @@ import java.util.stream.Stream;
  */
 public class Day2_Question43Multiply {
 
-    public String multiply(String num1, String num2) {
+    // 我的想法是先逆字符串，这样可以保证可以进位
+    public static String multiply(String num1, String num2) {
         String str1 = revertString(num1);
         String str2 = revertString(num2);
         int len1 = str1.length();
         int len2 = str2.length();
-        int maxlen = len1 > len2 ? len1 : len2;
-        int minlen = len1 + len2 - maxlen;
-        StringBuilder resStr = new StringBuilder();
-        for (int i = 0; i < minlen; i++) {
-            for (int j = 0; j < maxlen; j++) {
-                int num = (num1.charAt(i) - '0') * (num1.charAt(j) - '0');
-                
+        int[] nums = new int[len1 + len2 + 1];
+        for (int i = 0; i < len1; i++) {
+            for (int j = 0; j < len2; j++) {
+                int num = (str1.charAt(i) - '0') * (str2.charAt(j) - '0');
+                nums[i + j] += num;
             }
         }
-        return "";
+        int inex = 0;
+        for (int i = 0; i < len1 + len2 + 1; i++) {
+            nums[i] += inex;
+            inex = nums[i] / 10;
+            nums[i] %= 10;
+        }
+        StringBuilder resStr = new StringBuilder();
+        boolean flag = true;
+        for (int i = len1 + len2; i >= 0 ; i--) {
+            if (nums[i] == 0 && flag){
+                continue;
+            } else {
+                flag = false;
+            }
+            if (!flag){
+                resStr.append(nums[i]);
+            }
+        }
+        if (flag) resStr.append("0");
+        return resStr.toString();
     }
 
-    public String revertString(String str){
+    public static String revertString(String str) {
         StringBuilder stri = new StringBuilder();
-        for (int i = str.length() - 1; i  >= 0 ; i--) {
+        for (int i = str.length() - 1; i >= 0; i--) {
             stri.append(str.charAt(i));
         }
         return stri.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(multiply("123", "456"));
     }
 
 }
