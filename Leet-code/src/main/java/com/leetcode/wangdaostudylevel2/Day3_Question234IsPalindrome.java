0b9138fe1cccba199c5340b7910f3275d3d9ab60
heqiang1234 <1401651730@qq.com>
2022-08-31 23:29:30 +0800

    链表复习

---
+++ Leet-code/src/main/java/com/leetcode/wangdaostudylevel2/Day3_Question234IsPalindrome.java
@@ -0,0 +1,19 @@
+package com.leetcode.wangdaostudylevel2;
+
+import com.leetcode.model.ListNode;
+
+/**
+ * @author HQ
+ * @program: java-coding
+ * @description: 234.回文链表
+ * @date 2022-08-31 23:09:44
+ */
+public class Day3_Question234IsPalindrome {
+
+    public boolean isPalindrome(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast.next)
+    }
+
+}
