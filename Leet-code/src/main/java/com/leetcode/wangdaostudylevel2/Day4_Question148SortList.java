976b9b64914a7a116e79acec7f9b430b2e34a416
HQAQ <1401651730@qq.com>
2022-09-02 10:59:40 +0800

    链表的归并排序，先取二分到极致，一对一比较，然后排序。

--- Leet-code/src/main/java/com/leetcode/wangdaostudylevel2/Day4_Question148SortList.java
+++ Leet-code/src/main/java/com/leetcode/wangdaostudylevel2/Day4_Question148SortList.java
@@ -11,7 +11,52 @@ import com.leetcode.model.ListNode;
 public class Day4_Question148SortList {
 
     public ListNode sortList(ListNode head) {
-        return null;
+        return mergeList(head,null);
+    }
+
+    //归并排序
+    public ListNode mergeList(ListNode start,ListNode end){
+        if (start == null){
+            return null;
+        }
+        if (start.next == end){
+            start.next = null;
+            return start;
+        }
+        ListNode midNode = getMid(start,end);
+        ListNode list1 = mergeList(start,midNode);
+        ListNode list2 = mergeList(midNode,end);
+        ListNode res = getMgNode(list1,list2);
+        return res;
+    }
+
+    public ListNode getMgNode(ListNode list1,ListNode list2){
+        ListNode hd = new ListNode(-1);
+        ListNode curr = hd;
+        while (list1 != null && list2 != null){
+            if (list1.val >= list2.val){
+                curr.next = list2;
+                list2 = list2.next;
+            } else {
+                curr.next = list1;
+                list1 = list1.next;
+            }
+        }
+        curr.next = list1 == null ? list2 : list1;
+        return hd.next;
+    }
+
+    public ListNode getMid(ListNode root,ListNode tail){
+        ListNode fast = root;
+        ListNode slow = root;
+        while (fast.next != tail){
+            fast = fast.next;
+            slow = slow.next;
+            if (fast.next != tail){
+                fast = fast.next;
+            }
+        }
+        return slow;
     }
 
 }
