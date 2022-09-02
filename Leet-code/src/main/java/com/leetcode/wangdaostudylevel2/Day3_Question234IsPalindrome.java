3306194d5e4b12cc4c6c8e6817bd7d7133641b8c
HQAQ <1401651730@qq.com>
2022-09-02 11:00:23 +0800

    回文链表，取到中间的那个节点，然后翻转后面的链表。

--- Leet-code/src/main/java/com/leetcode/wangdaostudylevel2/Day3_Question234IsPalindrome.java
+++ Leet-code/src/main/java/com/leetcode/wangdaostudylevel2/Day3_Question234IsPalindrome.java
@@ -11,9 +11,38 @@ import com.leetcode.model.ListNode;
 public class Day3_Question234IsPalindrome {
 
     public boolean isPalindrome(ListNode head) {
-        ListNode fast = head;
-        ListNode slow = head;
-        while (fast.next)
+        ListNode midNode = getMidListNode(head);
+        ListNode revertNode = revertListNode(midNode);
+        while (revertNode != null && head != null){
+            if (head.val != revertNode.val){
+                return false;
+            }
+            revertNode = revertNode.next;
+            head = head.next;
+        }
+        return true;
+    }
+
+    public ListNode revertListNode(ListNode root){
+        ListNode pre = null;
+        ListNode curr = root;
+        while (curr != null){
+            ListNode now = curr.next;
+            curr.next = pre;
+            pre = curr;
+            curr = now;
+        }
+        return pre;
+    }
+
+    public ListNode getMidListNode(ListNode root){
+        ListNode fast = root;
+        ListNode slow = root;
+        while (fast.next != null && fast.next.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
     }
 
 }
