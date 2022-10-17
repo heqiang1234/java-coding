560eb1500c33c1d14ed22d04b93b2e9084e6d05c
HQAQ <1401651730@qq.com>
2022-10-17 15:18:55 +0800

    Revert "static字段添加"
    This reverts commit 300957f94e481530d9a36f56363704f2e0023270.

--- Leet-code/src/main/java/com/leetcode/daimasuixianglu/Day02_977SortedSquares.java
+++ Leet-code/src/main/java/com/leetcode/daimasuixianglu/Day02_977SortedSquares.java
@@ -8,7 +8,7 @@ package com.leetcode.daimasuixianglu;
  */
 public class Day02_977SortedSquares {
 
-    public static int[] sortedSquares(int[] nums){
+    public int[] sortedSquares(int[] nums){
         int count = 0;//统计负数
         int len = nums.length - 1;
         for (int i = 0; i <= len; i++) {
